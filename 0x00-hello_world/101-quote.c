344910c0c5acf502608427bb210844cc3a326c49
AHMED <ahmed.salah200555@gmail.com>
2023-03-16 08:40:57 -0700

    adv2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char a[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDOUT_FILENO, a , sizeof(a)-1 );
+	return (1);
+}
