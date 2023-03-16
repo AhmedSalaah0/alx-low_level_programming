5660b9e53d7d6e766c016632e2bf9e9f0312e9f2
AHMED <ahmed.salah200555@gmail.com>
2023-03-16 08:45:40 -0700

    adv2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDOUT_FILENO,a,sizeof(a) - 1 );
+	write(STDOUT_FILENO, a, sizeof(a) - 1);
 	return (1);
 }
