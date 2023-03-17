4988106730a3aac9a96f473c57583664ec09dafa
AHMED <ahmed.salah200555@gmail.com>
2023-03-17 09:08:48 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char a = 'a';
+
+while (a <= 122)
+{
+if (a == 'q' || a == 'e'){
+a++;
+}
+else
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
