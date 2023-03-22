08cae06fa6c5a6befbaa9607c85897a58383c868
AHMED <ahmed.salah200555@gmail.com>
2023-03-22 13:17:16 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void  print_alphabet_x10()
+{
+int i = 0;
+
+while (i < 10)
+{
+for(int l = 'a' ; l <= 'z'; l++)
+{
+_putchar(l);
+}
+i++;
+}
