7065867bf8f57014a94f1929cb3bb0098a048c4f
AHMED <ahmed.salah200555@gmail.com>
2023-03-27 08:50:30 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdout>
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+puts(*str);
+puts("\n");
+}
