039f8917e458159ea84f59f7973c55134b78fb5e
AHMED <ahmed.salah200555@gmail.com>
2023-03-28 16:52:44 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,15 +9,5 @@
  */
 void print_rev(char *s)
 {       
-int i;
-char rev[strlen(s)-1];
-int j;
-
-for (i = strlen(s); i >= 0; i--)
-{
-rev[strlen(s)-i] = s[i-1];
-}
-for (j = 0 ; j < strlen(s);j++)
-s[j] = rev[j];
-}printf("\n");
+strrev(s);
 }
