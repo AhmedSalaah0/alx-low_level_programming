6e879f7602f1a8a8ceba4563dad8c7747d1b7dda
AHMED <ahmed.salah200555@gmail.com>
2023-03-29 03:25:32 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,18 +7,19 @@
  *
  * then print new line
  */
-void puts_half(char *str){
-int i; 
+void puts_half(char *str)
+{
+int i;
 int len = strlen(str);
 
-if (len%2 == 0)
+if (len % 2 == 0)
 {
-for (i = len/2 ; i < len ; i++)
+for (i = len / 2 ; i < len && i != '\0' ; i++)
 printf("%c", str[i]);
 }
 else
 {
-for (i = (len - 1) / 2 ; i < len ; i++)
+for (i = (len - 1) / 2 ; i < len && i != '\0' ; i++)
 printf("%c", str[i]);
 }
 printf("\n");
