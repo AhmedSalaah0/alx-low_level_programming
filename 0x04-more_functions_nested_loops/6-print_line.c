23cab5163159be39d0a4b363dcc7c0196b68030c
AHMED <ahmed.salah200555@gmail.com>
2023-04-04 05:32:23 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - print number of _
+ * @n: num of _
+ * followed by new line
+*/
+void print_line(int n)
+{
+int i = 0;
+
+while (n--)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
