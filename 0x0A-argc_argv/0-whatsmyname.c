9f508b7cd526561524ff61b24b41a23f003705b0
AHMED <ahmed.salah200555@gmail.com>
2023-04-12 21:00:43 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *@argc:num of string index
+ *@argv: pointar contain string
+ * Return: return 0
+ */
+int main(int argc, int *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
