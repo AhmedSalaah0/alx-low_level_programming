6662222b8cf0de26a2761ef399766eb5b948d55a
AHMED <ahmed.salah200555@gmail.com>
2023-04-17 06:32:37 -0700

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,11 @@ struct dog
 char *name;
 float age;
 char *owner;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
 #endif
