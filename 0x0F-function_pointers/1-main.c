a1b4a543bc5c1cfe029f801550de2a46b80197a6
AHMED <ahmed.salah200555@gmail.com>
2023-04-20 00:50:56 -0700

    1

--- 0x0F-function_pointers/1-main.c
+++ 0x0F-function_pointers/1-main.c
@@ -1,38 +1,16 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
-/**
- * print_elem - prints an integer
- * @elem: the integer to print
- *
- * Return: Nothing.
- */
 void print_elem(int elem)
 {
     printf("%d\n", elem);
 }
 
-/**
- * print_elem_hex - prints an integer, in hexadecimal
- * @elem: the integer to print
- *
- * Return: Nothing.
- */
-void print_elem_hex(int elem)
-{
-    printf("0x%x\n", elem);
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    int array[5] = {0, 98, 402, 1024, 4096};
+    int array[] = {1, 2, 3, 4, 5};
+    size_t size = sizeof(array) / sizeof(int);
 
-    array_iterator(array, 5, &print_elem);
-    array_iterator(array, 5, &print_elem_hex);
-    return (0);
+    array_iterator(array, size, print_elem);
+
+    return 0;
 }
