eebc23c908a58086eaf2f51bf1a56904b078ad9d
AHMED <ahmed.salah200555@gmail.com>
2023-04-23 09:22:17 -0700

    3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *alloc_grid - function
+ * @width: array width
+ * @height: array height
+ * Return: NULL or g(pointer)
+ */
+int **alloc_grid(int width, int height)
+{
+int **g, i, j;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+g = malloc(height * sizeof(int *));
+
+if (g == NULL)
+return (NULL);
+
+for (i = 0; i < height; i++)
+{
+	g[i] = calloc(width, sizeof(int));
+	if (g[i] == NULL)
+	{
+		for (j = 0; j < i; j++)
+			free(g[j]);
+		free(g);
+		return (NULL);
+	}
+}
+return (g);
+}
