927e7766a4739c7418fc203356830ebee9412cb5
AHMED <ahmed.salah200555@gmail.com>
2023-04-22 21:32:12 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdub - function dublicates a string
+ * @str: string
+ * Return: Null or pointer
+ */
+
+char *_strdup(char *str)
+{
+char *s;
+unsigned int i, l;
+
+if (str == NULL)
+return (NULL);
+
+l = 0;
+while (str[l] != '\0')
+l++;
+
+s = malloc(sizeof(char) * (l + 1));
+
+if (s == NULL)
+return (NULL);
+
+for (i = 0; i < l; i++)
+s[i] = str[i];
+
+s[l] = '\0';
+
+return (s);
+}
