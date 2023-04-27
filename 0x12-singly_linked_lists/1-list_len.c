bfb6376b823b540579f3b2f898ed4913f98a2ced
AhmedSalaah0 <ahmed.salah200555@gmail.com>
2023-04-27 03:35:27 -0700

    1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- *
- *
+ *list_len - func return list length
+ *@h: pointer to a head of list
+ *Return: list size
  */
 size_t list_len(const list_t *h)
 {
