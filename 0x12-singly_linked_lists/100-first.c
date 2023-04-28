724974d1500b720d4547bf02d376ba0e8d2c6ccd
AhmedSalaah0 <ahmed.salah200555@gmail.com>
2023-04-27 18:54:14 -0700

    100adv

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * before_main - function prints before main
+ */
+void __attribute__((constr)) before_main()
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
