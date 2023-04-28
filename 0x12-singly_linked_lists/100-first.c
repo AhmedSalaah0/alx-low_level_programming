92116ad4d861eb8afcbea3b55d693b53267324b3
AhmedSalaah0 <ahmed.salah200555@gmail.com>
2023-04-27 18:56:51 -0700

    100adv

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 /**
  * before_main - function prints before main
  */
-void __attribute__((constr)) before_main()
+void __attribute__((constructor)) before_main()
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
