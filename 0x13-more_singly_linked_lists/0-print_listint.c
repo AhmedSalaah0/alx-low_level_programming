34ca178a82d1741ccf9710fea011fd27b96a5773
AhmedSalaah0 <ahmed.salah200555@gmail.com>
2023-04-29 02:59:36 -0700

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,10 @@ size_t print_listint(const listint_t *h)
 {
 size_t c = 0;
 
-while (h)
+while (h != NULL)
 {
-	if (h->s != NULL)
-		printf("%d\n", h->n);
+	printf("%d\n", h->n);
+	h = h->next;
 	c++;
 }
 return (c);
