0f00b89fb3b4dbb9921f1f5c568547b1cc43a366
AhmedSalaah0 <ahmed.salah200555@gmail.com>
2023-05-01 07:27:57 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,17 +11,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, c;
+int i, c;
 
-	c = 0;
+c = 0;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-	i = atoi(argv[1]);
+i = atoi(argv[1]);
 
 if (i < 0)
 {
@@ -30,16 +30,16 @@ return (0);
 }
 while (i > 0)
 {
-	if (i >= 25)
-	i -= 25;
- 	else if (i >= 10)
-	i -= 10;
-	else if (i >= 50)
-	i -=5;
-	else if (i >= 2)
-	i -= 2;
-	else
-	i -= 1;
+if (i >= 25)
+i -= 25;
+else if (i >= 10)
+i -= 10;
+else if (i >= 50)
+i -= 5;
+else if (i >= 2)
+i -= 2;
+else
+i -= 1;
 c++;
 }
 printf("%d\n", c);
