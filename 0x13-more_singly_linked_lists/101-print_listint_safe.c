bf3d823dc62fd5ceff9f6fd73cf2693b5a596314
AhmedSalaah0 <ahmed.salah200555@gmail.com>
2023-05-02 11:14:00 -0700

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,7 +11,7 @@ size_t print_listint_safe(const listint_t *head)
 {
 size_t count = 0;
 const listint_t *curr = head, *prev = NULL;
-if (curr == NULL)
+if (head == NULL)
 {
 exit(98);
 return (count);
