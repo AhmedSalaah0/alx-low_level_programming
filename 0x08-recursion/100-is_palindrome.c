2642a780c686caaa56cdccd6f892f6ca647164a8
AhmedSalaah0 <ahmed.salah200555@gmail.com>
2023-05-02 14:57:10 -0700

    101

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <string.h>
 #include <ctype.h>
-
 /**
  * is_palindrome - check if string is palindrome
  * @s: string that while check
@@ -9,21 +8,9 @@
  */
 int is_palindrome(char *s)
 {
-    int l = strlen(s);
-    char *p = s;
-    char *q = s + l - 1;
-
-    while (p < q)
-    {
-        while (!isalpha(*p))
-            p++;
-        while (!isalpha(*q))
-            q--;
-        if (tolower(*p) != tolower(*q))
-            return (0);
-        p++;
-        q--;
-    }
-
-    return (1);
+if (strlen(s) <= 1)
+{
+return (1);
+}
+return tolower(s[0]) == tolower(s[strlen(s) - 1]) && is_palindrome(s + 1);
 }
