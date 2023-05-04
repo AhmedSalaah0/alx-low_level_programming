3a4730ff4d271e9ea37d4f20c73e433820a2cede
AhmedSalaah0 <ahmed.salah200555@gmail.com>
2023-05-04 14:21:39 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *print_binary - convert dec to bin
  *@n: dec number
@@ -8,5 +9,5 @@ void print_binary(unsigned long int n)
 if (n > 1)
 print_binary(n >> 1);
 
-_putchar((n & 1) + '0');
+putchar((n & 1) + '0');
 }
