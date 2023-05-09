320816928caeeaa866e8edc30640ed0dc6c751c7
AhmedSalaah0 <ahmed.salah200555@gmail.com>
2023-05-09 05:10:55 -0700

    1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 #endif
