855cdce4bbab9e416d2864e366d7759ed1f409dc
ahmedsalaah0 <ahmed.salah200555@gmail.com>
2023-07-06 18:40:48 +0300

    first

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 int res = 0, bs = 1, i, len = strlen(b);
 char *str = (char *)b;
 
-if (str == NULL)
+if (b == NULL)
 return (0);
 
 for (i = len - 1; i >= 0; i--)
