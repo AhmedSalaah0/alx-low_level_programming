9a05df52f9d6eafb54606203a475917575d8335e
ahmedsalaah0 <ahmed.salah200555@gmail.com>
2023-07-06 18:29:37 +0300

    first

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 /**
- *
- *
- *
+ *binary_to_uint - convert binary to decimal
+ *@b: binary num as a string
+ *Return: num after convert
  */
-int main(void)
-{
 
+
+unsigned int binary_to_uint(const char *b)
+{
+int res = 0, bs = 1, i ,len = strlen(b);
+char *str = (char *)b;
+
+for (i = len - 1; i >= 0; i--)
+{
+if (str[i] == '1')
+res += bs;
+bs *= 2;
+}
+return (res);
+}
