d38723071e1f4a46b9062e1f6e1aa17c814e5dbe
ahmedsalaah0 <ahmed.salah200555@gmail.com>
2023-07-06 21:31:40 +0300

    second

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,23 +7,8 @@
  */
 void print_binary(unsigned long int n)
 {
-    unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-    int print_flag = 0;
+if (n > 1)
+print_binary(n >> 1);
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
-
-    while (mask > 0)
-    {
-        if (n & mask || print_flag)
-        {
-            _putchar((n & mask) ? '1' : '0');
-            print_flag = 1;
-        }
-        mask >>= 1;
-    }
+putchar((n & 1) + '0');
 }
-
