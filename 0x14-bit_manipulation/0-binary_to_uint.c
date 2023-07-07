464de2c64a7bf5fab3771fa2965eab0c34b05e5b
ahmedsalaah0 <ahmed.salah200555@gmial.com>
2023-07-07 14:41:33 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,11 +13,7 @@ unsigned int binary_to_uint(const char *b)
 {
 int res = 0, bs = 1, len;
 
-<<<<<<< HEAD
 if (b == NULL)
-=======
-if (b == '\0')
->>>>>>> 665504cbf3a722b287b65fadf4fd2697ff80eda7
 return (0);
 
 len = strlen(b);
