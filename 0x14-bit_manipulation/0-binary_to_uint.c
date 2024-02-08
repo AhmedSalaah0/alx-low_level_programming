7d4ce616398a6dbf495fe3094ce5321631ef1cc1
Ahmed Salah <ahmed.salah200555@gmail.com>
2024-02-08 07:50:24 -0800

    add

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,19 +2,20 @@
 #include <math.h>
 #include <string.h>
 /**
- *
- *
+ *binary_to_uint - convert bin num to uint
+ *@b: bin num
+ *Return: converted num if can otherwise 0
  */
-unsigned int binary_to_uint(const char *b){
+unsigned int binary_to_uint(const char *b)
+{
 	int i = strlen(b), j;
 	unsigned int res = 0;
-	
-	if (b == NULL)
-	return (0);	
 
-	for (i--, j = 1; i >= 0; i--, j *= 2) 
+	if (b == NULL)
+	return (0);
+	for (i--, j = 1; i >= 0; i--, j *= 2)
 	{
-	if (b[i] == '1') 
+	if (b[i] == '1')
 	{
 	res += j;
 	}
