2c6a1c1bed56ef64dad234c6565c1c4d613bdb23
Ahmed Salah <ahmed.salah200555@gmail.com>
2024-02-12 17:22:42 +0200

    1-task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,22 +8,22 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int f, len;
+int f, len;
 
-    if (filename == NULL)
-    {
-        return (-1);
-    }
-    f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (filename == NULL)
+{
+return (-1);
+}
+f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    if (f == -1)
-    {
-        return (-1);
-    }
+if (f == -1)
+{
+return (-1);
+}
 
-    text_content = text_content == NULL ? "" : text_content;
+text_content = text_content == NULL ? "" : text_content;
 
-    len = strlen(text_content);
-    write(f, text_content, len);
-    return (1);
+len = strlen(text_content);
+write(f, text_content, len);
+return (1);
 }
